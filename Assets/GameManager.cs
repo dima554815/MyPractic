c39fd8d6bf4842557b509821efba9ff6f967cd86
dima554815 <dimailin09.08@gmail.com>
2025-04-26 19:44:17 +0300

    ferma stsrt

---
+++ Assets/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    
+    
+
+
+
+
+public Button peasantBatton;
+public Button warriorBatton;
+
+public Text resourcesText;
+
+public int peasantCount;
+public int warriorCount;
+public int wheatCount;
+
+public int wheatPerPeasant;
+public int wheatToWarriors;
+
+public int peasantCost;
+public int warriorCost;
+
+public float peasantCrreateTime;
+public float warriorCostTime;
+public float raidMaxTime;
+public int raidIncrease;
+public int nextRaid;
+
+private float peasantTimer = -2;
+private float warriorCostTimer = -2;
+private float raidMaxTimer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CreatePeasant()
+    {
+
+    }
+
+    public void CreateWarrior()
+    {
+
+    }
+
+    private void UpdateText()
+    {
+
+    }
+
+
+
+
+
+
+
+
+}
