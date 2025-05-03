dd14d1cac3b0d3a59b8fdbdcce4e8dafdc6d821a
dima554815 <dimailin09.08@gmail.com>
2025-05-03 19:49:17 +0300

    ferma

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,45 +5,55 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
-    
+    public ImageTimerHarvest HarvestTimer;
+    public TimerEating EatingTimer;
+    public Image RaidTimerImg;
+    public Image PeasantTimerImg;
+    public Image warriorTimerImg;
 
+    public Button peasantBatton;
+    public Button warriorBatton;
 
+    public Text resourcesText;
 
+    public int peasantCount;
+    public int warriorsCount;
+    public int wheatCount;
 
-public Button peasantBatton;
-public Button warriorBatton;
+    public int wheatPerPeasant;
+    public int wheatToWarriors;
 
-public Text resourcesText;
+    public int peasantCost;
+    public int warriorsCost;
 
-public int peasantCount;
-public int warriorCount;
-public int wheatCount;
+    public float peasantCreateTime;
+    public float warriorCreateTime;
+    public float raidMaxTime;
+    public int raidIncrease;
+    public int nextRaid;
 
-public int wheatPerPeasant;
-public int wheatToWarriors;
-
-public int peasantCost;
-public int warriorCost;
-
-public float peasantCrreateTime;
-public float warriorCostTime;
-public float raidMaxTime;
-public int raidIncrease;
-public int nextRaid;
-
-private float peasantTimer = -2;
-private float warriorCostTimer = -2;
-private float raidMaxTimer;
+    private float peasantTimer = -2;
+    private float warriorCostTimer = -2;
+    private float raidMaxTimer;
 
     void Start()
     {
-        
+        UpdateText();
     }
 
     void Update()
     {
-        
+        if(HarvestTimer.Tick)
+        {
+            wheatCount += peasantCount * wheatPerPeasant;
+        }
+
+        if(EatingTimer.Tick)
+        {
+            wheatCount += warriorsCount * wheatToWarriors;
+        }
+
+        UpdateText();
     }
 
     public void CreatePeasant()
@@ -58,7 +68,7 @@ private float raidMaxTimer;
 
     private void UpdateText()
     {
-
+        resourcesText.text = peasantCount + "\n" + warriorsCount + "\n\n" + wheatCount;
     }
 
 
